b5dd16f96ea1cf5a9ba04acd5411ea201d3e56cc
onikage <twsama@gmail.com>
2011-09-13 17:23:10 +0800

    Add get method by annotation part.

--- javabase/annotation/src/main/java/com/snk/annotation/target/TestMethodAnnotation.java
+++ javabase/annotation/src/main/java/com/snk/annotation/target/TestMethodAnnotation.java
@@ -1,9 +1,12 @@
 package com.snk.annotation.target;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface TestMethodAnnotation {
 	String name = "TestMethodAnnotation";
 	String testStr = "testStrValue";
--- javabase/annotation/src/test/java/com/snk/Test.java
+++ javabase/annotation/src/test/java/com/snk/Test.java
@@ -1,8 +1,10 @@
 package com.snk;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import com.snk.annotation.inherited.InheriteSub;
+import com.snk.annotation.target.TestMethodAnnotation;
 
 public class Test {
 	public static void main(String[] args) {
@@ -15,5 +17,28 @@ public class Test {
 		//TestInherited
 		Class<InheriteSub> is = InheriteSub.class;
 		System.out.println(is.getAnnotations()[0]);
+		
+		//get method by annotation
+		methods = TestTarget.class.getMethods();
+		for (int i = 0; i < methods.length; i++) {
+			Method m = methods[i];
+			if(null != m.getAnnotation(TestMethodAnnotation.class)){
+				System.out.print(m.getName()+"   ");
+				System.out.println(m.getAnnotation(TestMethodAnnotation.class));
+				
+				try {
+					m.invoke(new TestTarget(), new Object[]{});
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
